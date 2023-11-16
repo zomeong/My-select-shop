e379a72227421304466ee97ae7f91d9dd145986f
Kim Jihyun <legojhk@gmail.com>
2023-11-16 12:35:50 +0900

    관심 상품에 폴더 추가 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -38,4 +38,12 @@ public class ProductController {
             @AuthenticationPrincipal UserDetailsImpl userDetails){
         return productService.getProducts(userDetails.getUser(),page-1, size, sortBy, isAsc);
     }
+
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails){
+        productService.addFolder(productId, folderId, userDetails.getUser());
+    }
 }
